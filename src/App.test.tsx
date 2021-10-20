bf91af00f287eb2c197a21bc76487c4b3f0bafac
Dom Vinyard <domvinyard@me.com>
2021-10-20 13:21:30 +0200

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import useImage from 'react-use-image';
+jest.mock('react-use-image');
 
-test('renders learn react link', () => {
+// Ensure the loading text displays while the image is loading
+test('loading', async () => {
+  (useImage as any).mockImplementation(() => ({ loaded: false }));
   render(<App />);
-  const linkElement = screen.getByText(/ Send/i);
-  expect(linkElement).toBeInTheDocument();
+  const loadingElement = screen.getByText(/establishing connection/i);
+  expect(loadingElement).toBeInTheDocument();
+});
+
+// Ensure the app runs after the image loads
+test('app', async () => {
+  (useImage as any).mockImplementation(() => ({ loaded: true }));
+  render(<App />);
+  const sendButton = screen.getByText(/Send/i);
+  expect(sendButton).toBeInTheDocument();
+  expect(screen.getByTestId(/not-processing/i)).toBeInTheDocument();
+
+  // Click the send button and ensure the app starts processing
+  sendButton.click();
+  await waitFor(() => {
+    expect(screen.getByTestId(/is-processing/i)).toBeInTheDocument();
+  });
 });
